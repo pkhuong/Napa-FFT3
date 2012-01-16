ef27b48fa6468cac038dfd1aaea94e2e96bffd63
Paul Khuong <pvk@pvk.ca>
2012-01-16 12:44:51 -0500

    Smarter swap order at leaves too - hits ~100% bandwidth on delilah

--- bit-reversal.lisp
+++ bit-reversal.lisp
@@ -1,16 +1,106 @@
 (in-package "NAPA-FFT.GEN")
 
-(defun emit-small-bit-reverse (n &aux
-                                   (width (lb n)))
-  (assert (power-of-two-p n))
-  `(progn
-     ,@(loop
-         for i below n
-         for rev = (bit-reverse-integer i width)
-         when (< i rev)
-         collect
-         `(rotatef (aref vec (+ start ,i))
-                   (aref vec (+ start ,rev))))))
+(defvar *swap-block-size* 16)
+
+(defun emit-swaps-same (pairs builder)
+  (flet ((emit (pairs)
+           (let ((temps (make-hash-table))
+                 (swap  (make-hash-table))
+                 (locs  ()))
+             (map nil (lambda (pair)
+                        (destructuring-bind (a . b) pair
+                          (push a locs)
+                          (push b locs)
+                          (setf (gethash a swap) b
+                                (gethash b swap) a)))
+                  pairs)
+             (setf locs (sort locs #'<))
+             `(bblock
+                  ,(loop
+                     for loc in locs
+                     for temp
+                       = (setf (gethash loc temps)
+                               (make-symbol
+                                (format nil "SWAP[~A]"
+                                        (hash-table-count temps))))
+                     collect `((,temp) (t) ,(funcall builder loc))
+                     append
+                     (let ((writes '()))
+                       (loop
+                         (let* ((top   (car locs))
+                                (other (gethash top swap))
+                                (temp  (gethash other temps)))
+                           (unless temp
+                             (return (nreverse writes)))
+                           (pop locs)
+                           (push `(() ()
+                                   (setf ,(funcall builder top)
+                                         ,temp))
+                                 writes)))))
+                nil))))
+    (loop with block = (max (truncate *swap-block-size* 2) 1)
+          for start below (length pairs)
+            by block
+          for end   = (min (length pairs)
+                           (+ start block))
+          collect (emit (subseq pairs start end)))))
+
+(defun emit-swaps (pairs builder1 &optional (builder2 builder1))
+  (when (eql builder1 builder2)
+    (return-from emit-swaps (emit-swaps-same pairs builder1)))
+  (flet ((emit (pairs)
+           (when (= 1 (length pairs))
+             (return-from emit
+               (destructuring-bind (a . b) (elt pairs 0)
+                 `(let ((a ,(funcall builder1 a))
+                        (b ,(funcall builder2 b)))
+                    (setf ,(funcall builder1 a) b
+                          ,(funcall builder2 b) b)))))
+           (let ((temps (make-hash-table))
+                 (pairs (sort pairs #'< :key #'car)))
+             (map nil (lambda (pair)
+                        (setf (gethash pair temps)
+                              (make-symbol
+                               (format nil "SWAP[~A]"
+                                       (hash-table-count temps)))))
+                  pairs)
+             (let* ((last  (elt pairs
+                                (1- (length pairs))))
+                    (pairs (subseq pairs
+                                   0
+                                   (1- (length pairs))))
+                    (rev   (sort (copy-seq pairs)
+                                 #'< :key #'cdr)))
+               `(let ,(map 'list
+                       (lambda (pair)
+                         (let ((tmp (gethash pair temps)))
+                           `(,tmp ,(funcall builder1 (car pair)))))
+                       pairs)
+                  (let ,(map 'list
+                         (lambda (pair)
+                           (let ((tmp   (gethash pair temps))
+                                 (place (funcall builder2 (cdr pair))))
+                             `(,tmp (prog1 ,place
+                                      (setf ,place ,tmp)))))
+                         rev)
+                    ,(let ((first (elt pairs 0)))
+                       `(setf ,(funcall builder1 (car first))
+                              ,(gethash first temps)))
+                    (let ((a ,(funcall builder1 (car last)))
+                          (b ,(funcall builder2 (cdr last))))
+                      (setf ,(funcall builder1 (car last)) b
+                            ,(funcall builder2 (cdr last)) a))
+                    ,@(map 'list
+                           (lambda (pair)
+                             `(setf ,(funcall builder1 (car pair))
+                                    ,(gethash pair temps)))
+                           (subseq pairs 1))))))))
+    (loop with block = (max *swap-block-size* 1)
+          for start below (length pairs)
+            by block
+          for end   = (min (length pairs)
+                           (+ start block))
+          collect (emit (subseq pairs start end)))))
 
 ;; another way to z-order the bottom bits:
 (defun generate-outward-seq (seed most least)
@@ -53,6 +143,21 @@
       (sort interleaved #'< :key #'car)
       (map-into interleaved #'cdr interleaved))))
 
+(defun emit-small-bit-reverse (n &aux
+                                   (width (lb n)))
+  (assert (power-of-two-p n))
+  (let ((to-swap '()))
+    (dotimes (i n)
+      (let ((rev (bit-reverse-integer i width)))
+        (when (< i rev)
+          (push (cons i rev) to-swap))))
+    `(progn
+       ,@(emit-swaps (z-order-words to-swap
+                                    (1- (ash 1 (ceiling width 2))))
+                     (lambda (i)
+                       `(aref vec (+ start ,i))))
+       nil)))
+
 (defun generate-leaf-reverse (outer inner)
   (let ((to-swap '())
         (total    (+ outer inner)))
@@ -65,13 +170,11 @@
             (when (< a b)
               (push (cons a b) to-swap))))))
     `(progn
-       ,@(loop
-           for (a . b) across (z-order-words to-swap
-                                             (1- (ash 1 outer)))
-           collect `(let ((a (aref vec (+ middle ,a)))
-                          (b (aref vec (+ middle ,b))))
-                      (setf (aref vec (+ middle ,a)) b
-                            (aref vec (+ middle ,b)) a))))))
+       ,@(emit-swaps (z-order-words to-swap
+                                    (1- (ash 1 outer)))
+                     (lambda (i)
+                       `(aref vec (+ middle ,i))))
+       nil)))
 
 (defun generate-leaf-reverse-swap (outer inner)
   (let ((to-swap '())
@@ -84,14 +187,13 @@
                  (b  (+ (ash rj total) ri)))
             (push (cons a b) to-swap)))))
     `(progn
-       ,@(loop
-           for (a . b) across (z-order-words to-swap
-                                             (1- (ash 1 outer)))
-           collect
-           `(let ((a (aref vec (+ middle-1 ,a)))
-                  (b (aref vec (+ middle-2 ,b))))
-              (setf (aref vec (+ middle-1 ,a)) b
-                    (aref vec (+ middle-2 ,b)) a))))))
+       ,@(emit-swaps (z-order-words to-swap
+                                    (1- (ash 1 outer)))
+                     (lambda (i)
+                       `(aref vec (+ middle-1 ,i)))
+                     (lambda (i)
+                       `(aref vec (+ middle-2 ,i))))
+       nil)))
 
 (defun generate-large-reversal (outer inner)
   (let ((to-swap '()))
